bac5fae629ae0f36281932c47107d349ffebaa66
Federico Stra <stra.federico@gmail.com>
2022-02-21 11:33:57 +0100

    Add basic example that reads all TLEs under data/

---
+++ examples/read-all.rs
@@ -0,0 +1,27 @@
+use std::fs;
+use std::io::{self, prelude::*, BufReader};
+
+use itertools::Itertools;
+
+use tletools;
+
+fn main() -> io::Result<()> {
+    for date_folder in fs::read_dir("data")? {
+        for filename in fs::read_dir(date_folder?.path())? {
+            let filename = filename?;
+            println!("\n{:?}", filename.path());
+            let file = fs::File::open(filename.path())?;
+            let buf_reader = BufReader::new(file);
+            for mut chunk in &buf_reader.lines().chunks(3) {
+                let name = chunk.next().unwrap()?;
+                let line1 = chunk.next().unwrap()?;
+                let line2 = chunk.next().unwrap()?;
+                // let tle_str = [name.as_str(), line1.as_str(), line2.as_str()].join("\n");
+                let tle = tletools::from_lines(name.as_str(), line1.as_str(), line2.as_str());
+                println!("{:?}", tle);
+            }
+        }
+    }
+
+    Ok(())
+}
